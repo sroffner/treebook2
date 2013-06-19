e86541b0ea238ea451678a50a4d55e9960d2a49f
Sydney Offner <sydney.offner@gmail.com>
2013-06-18 23:45:29 -0400

    Added Simple Form

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130517035754) do
+ActiveRecord::Schema.define(:version => 20130521034315) do
 
   create_table "statuses", :force => true do |t|
-    t.string   "name"
     t.string   "string"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], :name => "index_statuses_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
